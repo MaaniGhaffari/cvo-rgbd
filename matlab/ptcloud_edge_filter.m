46362a8be65d6ffa84f518445809dc82fb82a8e1
Maani <maanigj@umich.edu>
2019-04-09 22:34:43 -0400

    fix removing nan values.

--- matlab/ptcloud_edge_filter.m
+++ matlab/ptcloud_edge_filter.m
@@ -8,7 +8,7 @@ X = reshape(cloud_in.Location,[], 3, 1);
 C = reshape(cloud_in.Color,[], 3, 1);
 X = X(reshape(BW, [], 1), :);
 C = C(reshape(BW, [], 1), :);
-X = X(~isnan(X(:,1)),:);
 C = C(~isnan(X(:,1)),:);
+X = X(~isnan(X(:,1)),:);
 
 clout_out = pointCloud(X, 'Color', C);
